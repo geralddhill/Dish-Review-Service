99c32fccb7963568c929fbabe25c299b88473c96
GetBlockt <gh.blockt9@gmail.com>
2023-11-28 22:08:03 -0800

    Implemented Ingredients class

--- src/Ingredient.cpp
+++ src/Ingredient.cpp
@@ -1,1 +1,45 @@
 #include "Ingredient.hpp"
+
+namespace dishReviewService {
+
+using std::string;
+using std::shared_ptr;
+using std::vector;
+
+Ingredient::Ingredient(string newDisplayName) {
+    displayName = newDisplayName;
+    isAllergen = false;
+}
+
+Ingredient::~Ingredient() {}
+
+void Ingredient::toggleAllergen() {
+    isAllergen = !isAllergen;
+}
+
+void Ingredient::addDish(shared_ptr<Dish> newDish) {
+    dishesUsedIn.push_back(newDish);
+}
+
+string Ingredient::getDisplayName() {
+    return displayName;
+}
+
+bool Ingredient::getAllergenStatus() {
+    return isAllergen;
+}
+
+vector<shared_ptr<Dish>> Ingredient::getDishesUsedIn() {
+    return dishesUsedIn;
+}
+
+std::ostream& operator<<(std::ostream& output, const Ingredient& outputIngredient) {
+    output << outputIngredient.displayName;
+    if (outputIngredient.isAllergen) {
+        output << " (Known Allergen)";
+    }
+    
+    return output;
+}
+
+}
--- src/Ingredient.hpp
+++ src/Ingredient.hpp
@@ -16,14 +16,15 @@ private:
     bool isAllergen;
     std::vector<std::shared_ptr<Dish>> dishesUsedIn; // Idk if this is how we want to do it
 public:
-    Ingredient();
+    Ingredient(std::string newDisplayName);
     ~Ingredient();
     
     void toggleAllergen();
+    void addDish(std::shared_ptr<Dish> newDish);
     
     std::string getDisplayName();
     bool getAllergenStatus();
-    std::vector<std::shared_ptr<Dish>> getDishesUsedIn;
+    std::vector<std::shared_ptr<Dish>> getDishesUsedIn();
     
     friend std::ostream& operator<<(std::ostream& output, const Ingredient& outputIngredient);
 };
